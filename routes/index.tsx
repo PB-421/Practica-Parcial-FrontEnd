5427aa4e5552425204efbaa3c8bf352d108e8f8a
Pablo B. A. <pablo.borderas@gmail.com>
2025-03-28 17:56:38 +0100

    Comentarios actualizados

--- routes/index.tsx
+++ routes/index.tsx
@@ -10,6 +10,7 @@ export const handler:Handlers = {
   GET: (req: Request,ctx:FreshContext<unknown,Data>) => {
       const url = new URL(req.url)
       const palabra = url.searchParams.get("Palabra") || null
+      //Redirect, muy util para rutas dinamicas que dependan de cosas de fromularios
       if(palabra) return new Response("",{status: 307,headers:{location: "/buscar/"+palabra} })
       return ctx.render()
   }
